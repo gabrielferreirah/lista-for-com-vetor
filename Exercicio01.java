020b55e51e3f34a658dc6c9fdf0b24fd15e03334
Gabriel de Oliveira Ferreira <38558405+gabrielferreirah@users.noreply.github.com>
2018-05-10 09:06:08 -0300

    CEDUP - 10/05

--- Exercicio01.java
+++ Exercicio01.java
@@ -1,5 +1,17 @@
-public class Exercicio01{
-    public static void main(String[] args) {
-        
-    }
+import javax.swing.JOptionPane;
+
+public class Exercicio01 {
+
+	public static void main(String[] args) {
+		String[] nomes = new String[25];
+		int i = 0;
+		for (i = 0; i < nomes.length; i++) {
+			nomes[i] = JOptionPane.showInputDialog(null, "Digite seu nome");
+			for (int j = i; j <= i; j++) {
+				System.out.println(nomes[i]);
+			}
+		}
+
+	}
+
 }
